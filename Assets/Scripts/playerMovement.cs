761629f75cae007ad81411349ffa043656cc4bca
Syed Ali Haider <56455410+roeticvampire@users.noreply.github.com>
2021-11-10 00:09:34 +0530

    fixed postFX

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -34,7 +34,7 @@ public class playerMovement : MonoBehaviour
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if(isGrounded && velocity.y < 0)
-        velocity.y=-2f;
+            velocity.y=-2f;
 
         float x=Input.GetAxis("Horizontal");
         float z=Input.GetAxis("Vertical");
